fb42f0ff7157a736713678e4d0a10ee60c9030d3
WijeAkshi <53735848+WijeAkshi@users.noreply.github.com>
2020-05-06 02:40:29 +0530

    PaymentDetails.java class added Payment.jsp edited

--- Payment_Mnanagement/WebContent/Payments.jsp
+++ Payment_Mnanagement/WebContent/Payments.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<%@ page import="model.PaymentDetails" %>
+	pageEncoding="ISO-8859-1"%>
+<%@ page import="model.PaymentDetails"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,35 +8,50 @@
 <title>Payment Management</title>
 <link rel="stylesheet" href="Views/bootstrap.min.css">
 <script src="Components/jquery-3.2.1.min.js"></script>
-<script src="Components/Payments.js"></script>
+<script src="Components/payments.js"></script>
 </head>
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-6">
+			
 				<h1>Payment Management</h1>
-				<form id="formItem" name="formItem">
-					Appointment no: <input id="appno" name="appno" type="text"
+				
+				<form id="formPayment" name="formPayment">
+				
+					Appointment no: <input id="app_Id" name="app_Id" type="text"
 						class="form-control form-control-sm"> <br>
-					 Card Type: <input id="cardtype" name="cardtype" type="text"
+						
+					 Card Type: <input id="cardType" name="cardType" type="text"
 						class="form-control form-control-sm"> <br>
+						
 					 Name on Card: <input id="nameOnCard" name="nameOnCard" type="text"
 						class="form-control form-control-sm"> <br>
+						
 				     Card no: <input id="cardno" name="cardno" type="text"
 						class="form-control form-control-sm"> <br>
+						
 					Phone: <input id="phone" name="phone" type="text"
 						class="form-control form-control-sm"> <br>
-					Expire date: <input id="expdate" name="expdate" type="text"
+						
+					Expire date: <input id="expdate" name="expdate" type="date"
 						class="form-control form-control-sm"> <br>
+						
 					Amount: <input id="amount" name="amount" type="text"
 						class="form-control form-control-sm"> <br>
+						
+					Status: <input id="status" name="status" type="text"
+						class="form-control form-control-sm"> <br>
+						
 					 <input id="btnSave" name="btnSave" type="button" value="Save"
 						class="btn btn-primary">
-					<input type="hidden"id="hidItemIDSave" name="hidItemIDSave" value="">
+					<input type="hidden"id="hidPaymentIDSave" name="hidPaymentIDSave" value="">
 				</form>
-				<div id="alertSuccess" ass="alert alert-danger"></div>
+				
+				<div id="alertSuccess" class="alert alert-success"></div>
+                <div id="alertError" class="alert alert-danger"></div>
 				<br>
-				<div id="divItemsGrid">
+				<div id="divPaymentGrid">
 					<%
 						PaymentDetails paymentdetails = new PaymentDetails();
 						out.print(paymentdetails.readPaymentDetails());
--- Payment_Mnanagement/src/model/PaymentDetails.java
+++ Payment_Mnanagement/src/model/PaymentDetails.java
@@ -1,41 +1,49 @@
 package model;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-import Util.DB_Connection;
 
 public class PaymentDetails {
-	
+	// A common method to connect to the DB
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
 
-	// insert new payment details
-	public String insertPaymentDetails(String app_Id, String ctype, String name, String cardno, String pho,
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/healthcare", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertPaymentDetails(String app_Id, String cardType, String nameOnCard, String cardno, String phone,
 			String expdate, String amount, String status) {
 		String output = "";
 		try {
-			DB_Connection con = new DB_Connection();
-			Connection conn = con.connect();
+			Connection con = connect();
 
 			if (con == null) {
 				return "Error while connecting to the database for inserting.";
 			}
 			// create a prepared statement
-			String query = " insert into paymentdetails (`id`,`app_Id`,`cardType`,`nameOnCard`,`cardno`,`phone`,`expdate`,`amount`)"
-					+ " values (?,?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			String query = " insert into paymentdetails (`id`,`app_Id`,`cardType`,`nameOnCard`,`cardno`,`phone`,`expdate`,`amount`,`status`)"
+					+ " values (?,?, ?, ?, ?, ?, ?, ?,?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 			preparedStmt.setInt(1, 0);
 			preparedStmt.setString(2, app_Id);
-			preparedStmt.setString(3, ctype);
-			preparedStmt.setString(4, name);
+			preparedStmt.setString(3, cardType);
+			preparedStmt.setString(4, nameOnCard);
 			preparedStmt.setString(5, cardno);
-			preparedStmt.setString(6, pho);
+			preparedStmt.setString(6, phone);
 			preparedStmt.setDate(7, java.sql.Date.valueOf(expdate));
 			preparedStmt.setDouble(8, Double.parseDouble(amount));
+			preparedStmt.setString(9, status);
 
 			// execute the statement
 			preparedStmt.execute();
-			conn.close();
+			con.close();
 			String newPaymentDetails  = readPaymentDetails();
 			output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
 		} catch (Exception e) {
@@ -45,116 +53,109 @@ public class PaymentDetails {
 		return output;
 	}
 
-	// retrieve payment details that was made by the patients
-	public String readPaymentDetails() {
-		String output = "";
-		try {
-			DB_Connection con = new DB_Connection();
-			Connection conn = con.connect();
-			if (con == null) {
-				return "Error while connecting to the database for reading.";
-			}
-			// Prepare the html table to be displayed
-			output = "<table border=\"1\"><tr> <th>AppoID</th> <th>CarsType</th> <th>Name</th> <th>cardno</th> <th>Phone</th ><th>Exp_date</th> <th>Amount</th> <th>Status</th><th>Update</th><th>Remove</th> </tr>";
-			String query = "select * from paymentdetails";
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			// iterate through the rows in the result set
-			while (rs.next()) {
-				String id = Integer.toString(rs.getInt("id"));
-				String app_Id = rs.getString("app_Id");
-				String cardType = rs.getString("cardType");
-				String nameOnCard = rs.getString("nameOnCard");
-				String cardno = rs.getString("cardno");
-				String phone = Integer.toString(rs.getInt("phone"));
-				String expdate = Integer.toString(rs.getInt("expdate"));
-				String amount = Double.toString(rs.getDouble("amount"));
-				String status = rs.getString("status");
-				// Add into the html table
-				output += "<tr><td><input id='hidIDUpdate' name='hidIDUpdate' type='hidden' value='" + id + "'>"
-				 + app_Id + "</td>";
-				output += "<td>" + cardType + "</td>";
-				output += "<td>" + nameOnCard + "</td>";
-				output += "<td>" + cardno + "</td>";
-				output += "<td>" + phone + "</td>";
-				output += "<td>" + expdate + "</td>";
-				output += "<td>" + amount + "</td>";
-				output += "<td>" + status + "</td>";
-				// buttons
-				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
-						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-itemid='"
-						+ id + "'>" + "</td></tr>";
+		public String readPaymentDetails() {
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for reading.";
+				}
+				// Prepare the html table to be displayed
+				output = "<table border=\"1\"><tr> <th>AppoID</th> <th>CarsType</th> <th>Name</th> <th>cardno</th> <th>Phone</th ><th>Exp_date</th> <th>Amount</th> <th>Status</th><th>Update</th><th>Remove</th> </tr>";
+				String query = "select * from paymentdetails";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next()) {
+					String id = Integer.toString(rs.getInt("id"));
+					String app_Id = rs.getString("app_Id");
+					String cardType = rs.getString("cardType");
+					String nameOnCard = rs.getString("nameOnCard");
+					String cardno = rs.getString("cardno");
+					String phone = Integer.toString(rs.getInt("phone"));
+					Date expdate = rs.getDate("expdate");
+					String amount = Double.toString(rs.getDouble("amount"));
+					String status = rs.getString("status");
+					// Add into the html table
+					output += "<tr><td><input id='hidIDUpdate' name='hidIDUpdate' type='hidden' value='" + id + "'>"
+					 + app_Id + "</td>";
+					output += "<td>" + cardType + "</td>";
+					output += "<td>" + nameOnCard + "</td>";
+					output += "<td>" + cardno + "</td>";
+					output += "<td>" + phone + "</td>";
+					output += "<td>" + expdate + "</td>";
+					output += "<td>" + amount + "</td>";
+					output += "<td>" + status + "</td>";
+					// buttons
+					output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+							+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-paymentid='"
+							+ id + "'>" + "</td></tr>";
 
+				}
+				con.close();
+				// Complete the html table
+				output += "</table>";
+			} catch (Exception e) {
+				output = "Error while reading the card details.";
+				System.err.println(e.getMessage());
 			}
-			conn.close();
-			// Complete the html table
-			output += "</table>";
-		} catch (Exception e) {
-			output = "Error while reading the card details.";
-			System.err.println(e.getMessage());
-		}
-		return output;
+			return output;
 	}
 
-	// update the status of payments
-	public String updatePaymentDetails(String id, String app_Id, String ctype, String name, String cardno, String pho, String expdate, String amount, String status) {
-		String output = "";
-		try {
-			DB_Connection con = new DB_Connection();
-			Connection conn = con.connect();
-			if (con == null) {
-				return "Error while connecting to the database for updating.";
-			}
-			// create a prepared statement
-			String query = "UPDATE paymentdetails SET app_Id=?,cardType=?,nameOnCard=?,cardno=?,phone=?,expdate=?,amount=?,status=? WHERE id=?";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-			// binding values
-			preparedStmt.setString(1, app_Id);
-			preparedStmt.setString(2, ctype);
-			preparedStmt.setString(3, name);
-			preparedStmt.setString(4, cardno);
-			preparedStmt.setInt(5, Integer.parseInt(pho));
-			preparedStmt.setDate(6, java.sql.Date.valueOf(expdate));
-			preparedStmt.setDouble(7, Double.parseDouble(amount));
-			preparedStmt.setString(8, status);
-			preparedStmt.setInt(9, Integer.parseInt(id));
+		public String updatePaymentDetails(String id, String app_Id, String cardType, String nameOnCard, String cardno, String phone, String expdate, String amount, String status) {
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for updating.";
+				}
+				// create a prepared statement
+				String query = "UPDATE paymentdetails SET app_Id=?,cardType=?,nameOnCard=?,cardno=?,phone=?,expdate=?,amount=?,status=? WHERE id=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				// binding values
+				preparedStmt.setString(1, app_Id);
+				preparedStmt.setString(2, cardType);
+				preparedStmt.setString(3, nameOnCard);
+				preparedStmt.setString(4, cardno);
+				preparedStmt.setInt(5, Integer.parseInt(phone));
+				preparedStmt.setDate(6, java.sql.Date.valueOf(expdate));
+				preparedStmt.setDouble(7, Double.parseDouble(amount));
+				preparedStmt.setString(8, status);
+				preparedStmt.setInt(9, Integer.parseInt(id));
 
-			// execute the statement
-			preparedStmt.execute();
-			conn.close();
-			String newPaymentDetails = readPaymentDetails();
-			output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
-		} catch (Exception e) {
-			output = "{\"status\":\"error\", \"data\": \"Error while updating the payment details.\"}";
-			System.err.println(e.getMessage());
-		}
-		return output;
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				String newPaymentDetails = readPaymentDetails();
+				output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
+			} catch (Exception e) {
+				output = "{\"status\":\"error\", \"data\": \"Error while updating the payment details.\"}";
+				System.err.println(e.getMessage());
+			}
+			return output;
 	}
 
-	// delete if a payment is not valid
-	public String deletePaymentDetails(String id) {
-		String output = "";
-		try {
-			DB_Connection con = new DB_Connection();
-			Connection conn = con.connect();
-			if (con == null) {
-				return "Error while connecting to the database for deleting.";
+		public String deletePaymentDetails(String id) {
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for deleting.";
+				}
+				// create a prepared statement
+				String query = "delete from paymentdetails where id=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				// binding values
+				preparedStmt.setInt(1, Integer.parseInt(id));
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				String newPaymentDetails = readPaymentDetails();
+				output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
+			} catch (Exception e) {
+				output = "Error while deleting the card details.";
+				System.err.println(e.getMessage());
 			}
-			// create a prepared statement
-			String query = "delete from paymentdetails where id=?";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-			// binding values
-			preparedStmt.setInt(1, Integer.parseInt(id));
-			// execute the statement
-			preparedStmt.execute();
-			conn.close();
-			String newPaymentDetails = readPaymentDetails();
-			output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
-		} catch (Exception e) {
-			output = "Error while deleting the card details.";
-			System.err.println(e.getMessage());
-		}
-		return output;
+			return output;
 	}
-
 }
